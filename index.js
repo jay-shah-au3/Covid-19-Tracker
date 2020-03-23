8c9d940cccf07ab66369ff144b391be9e15a5f25
jay-shah-au3 <shhahjay@gmail.com>
2020-03-23 14:21:54 +0530

    Update server

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const app = express();
 const cases = require('./server/routes/cases');
 const cors = require('cors');
 
+require("dotenv").config();
+const PORT = process.env.PORT;
+
 const cronJob = require('./server/cron');
 cronJob.cron1();
 cronJob.cron2();
 app.use(cors());
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+    app.get(/^\/(?!api).*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });    
+}
+
 app.use('/api/cases',cases);
 
-app.listen(8080);
+app.listen(PORT || 8080);
